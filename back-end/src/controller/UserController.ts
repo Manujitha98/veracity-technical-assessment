283a7dc18232fbf3a3d987426209ae53d88c12e1
Manujitha98 <manujithaperera@gmail.com>
2023-02-28 18:54:04 +0530

    added response status codes for user controller added authentication for fetcing single user endpoint

--- back-end/src/controller/UserController.ts
+++ back-end/src/controller/UserController.ts
@@ -8,11 +8,14 @@ export class UserController {
   private userRepository = AppDataSource.getRepository(User);
 
   async all(request: Request, response: Response, next: NextFunction) {
-    return this.userRepository.find();
+    return (await this.userRepository.find()).filter((user) => {
+      delete user.password;
+      return user;
+    });
   }
 
   async one(request: Request, response: Response, next: NextFunction) {
-    const id = parseInt(request.params.id);
+    const id = parseInt(request.user.id);
 
     //fetch the user data along with the wish list items
     const user = await this.userRepository.findOne({
@@ -21,10 +24,12 @@ export class UserController {
     });
 
     if (!user) {
+      response.staus(404);
       return "unregistered user";
     }
     //remove the password field from the response
     delete user.password;
+    response.status(200);
     return user;
   }
 
@@ -42,23 +47,28 @@ export class UserController {
       password: hashedPassword,
     });
 
+    //save the user
     const saveduser = await this.userRepository.save(user);
-    console.log(saveduser);
     const token = generateAuthToken(saveduser);
+    response.status(200);
     return token;
   }
 
   async remove(request: Request, response: Response, next: NextFunction) {
     const id = parseInt(request.params.id);
 
+    //find the user
     let userToRemove = await this.userRepository.findOneBy({ id });
 
+    //check if the user exists
     if (!userToRemove) {
+      response.status(404);
       return "this user not exist";
     }
 
+    //remove user
     await this.userRepository.remove(userToRemove);
-
+    response.status(200);
     return "user has been removed";
   }
 }
