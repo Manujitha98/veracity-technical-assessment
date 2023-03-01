a9bccbb191afcf0aa9162591bd668b16ed38ef8b
Manujitha98 <manujithaperera@gmail.com>
2023-03-01 15:03:39 +0530

    progress on the base componets added Button wrapper component added navbar, home page , login , sign up , error page  place holders added utils for protected routes and logout

---
+++ front-end/src/components/navbar.jsx
@@ -0,0 +1,53 @@
+import { Link, Outlet } from "react-router-dom";
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          Home
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup">
+                Sign up
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/profile">
+                <img
+                  src="logo192.png"
+                  alt="Profile Picture"
+                  className="rounded-circle"
+                  style={{ width: "30px" }}
+                />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
---
+++ front-end/src/utils/Logout.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+// import { logout } from "../../services/authService";
+
+function Logout() {
+  useEffect(() => {
+    // logout();
+    window.location.href = "/login";
+  });
+  return null;
+}
+
+export default Logout;
