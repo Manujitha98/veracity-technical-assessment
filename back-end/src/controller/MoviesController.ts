e890200f32a0f729b2eeb8e404c277857d555a5a
Manujitha98 <manujithaperera@gmail.com>
2023-03-01 12:50:08 +0530

    Added movies controller Added axios for cleaner http requests set up the Movies_API key as an environement variable. code refactoring to a service layer is still pending but its good to go for now

---
+++ back-end/src/controller/MoviesController.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import axios from "axios";
+
+export class MoviesController {
+  async getAllMovies(request: Request, response: Response, next: NextFunction) {
+    try {
+      //fetch a list of movies from api.themoviedb.org and return a list of movies
+      const movies = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIES_API_KEY}&language=en-US&page=${request.query.page}`
+      );
+      response.status(200);
+      return movies.data;
+    } catch (error) {
+      response.status(500);
+      return { error: "Something went wrong" };
+    }
+  }
+
+  async getMovieById(request: Request, response: Response, next: NextFunction) {
+    try {
+      //fetch a movie from api.themoviedb.org and return a movie
+      const movies = await axios.get(
+        `https://api.themoviedb.org/3/movie/${request.params.id}?api_key=${process.env.MOVIES_API_KEY}&language=en-US`
+      );
+      response.status(200);
+      return movies.data;
+    } catch (error) {
+      if (error.response.status === 404) {
+        response.status(404);
+        return { error: "Movie not found" };
+      }
+      response.status(500);
+      return { error: "Something went wrong" };
+    }
+  }
+}
