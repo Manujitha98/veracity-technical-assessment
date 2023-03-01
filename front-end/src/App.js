22ac69edbaffe3a7aa45457d2107ad45418185d5
Manujitha98 <manujithaperera@gmail.com>
2023-03-01 13:14:42 +0530

    set up packages for components , pages , assets , services and utilities added axios , toastify , and react router cleaned up boiler plate code added favicon and image assets for logos removed testing boiler plate code

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import "bootswatch/dist/cosmo/bootstrap.min.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+function App() {
+  return <h1> Hello World </h1>;
+}
+
+export default App;
