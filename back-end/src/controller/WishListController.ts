6fc5e6236422c8297aa5be85cd797cc3575810c9
Manujitha98 <70479833+Manujitha98@users.noreply.github.com>
2023-03-01 09:23:06 +0530

    added authentication for removing wish list routes (#4)
    added endpoint validations to prevent duplicate requests
    added the correct status codes for the response
    commented code for clarity
    :rocket: .... progress on dev

--- back-end/src/controller/WishListController.ts
+++ back-end/src/controller/WishListController.ts
@@ -8,7 +8,17 @@ export class WishListItemController {
   async add(request: Request, response: Response, next: NextFunction) {
     const { movieId } = request.body;
 
-    //TODO: check if the movie exists in the users wish list
+    //check if the movie exists in the users wish list
+    const wishListItemFound = await this.wishListItemRepository.findOneBy({
+      movieId: parseInt(movieId),
+      user: request.user.id,
+    });
+    //if the movie exists in the wish list, return an error message
+    if (wishListItemFound) {
+      response.status(400);
+      return "this movie already exists in your wish list";
+    }
+    //create a new wish list item
     const wishListItem = Object.assign(new WishListItem(), {
       movieId: parseInt(movieId),
       user: request.user.id,
@@ -18,18 +28,23 @@ export class WishListItemController {
   }
 
   async remove(request: Request, response: Response, next: NextFunction) {
+    //The id of the movie to be removed is taken from url param
     const id = parseInt(request.params.id);
 
+    //check if the user has the movie in his wish list
     let wishListItemToRemove = await this.wishListItemRepository.findOneBy({
-      id,
+      movieId: id,
+      user: request.user.id,
     });
 
+    //if the movie does not exist in the wish list, return an error message
     if (!wishListItemToRemove) {
+      response.status(404);
       return "this wish list entry does not exist";
     }
 
+    //remove the movie from the wish list
     await this.wishListItemRepository.remove(wishListItemToRemove);
-
     return "wish list item removed";
   }
 }
